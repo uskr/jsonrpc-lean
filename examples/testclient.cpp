346f68a7bdab92deacddff242589d1b168eda37d
a a <a a>
2017-04-25 09:42:34 +0800

    fix for mingw g++ compile

--- examples/testclient.cpp
+++ examples/testclient.cpp
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
+#define __USE_MINGW_ANSI_STDIO 0
 #include "../include/jsonrpc-lean/client.h"
 #include "../include/jsonrpc-lean/fault.h"
 #include "../include/jsonrpc-lean/jsonformathandler.h"
--- examples/testserver.cpp
+++ examples/testserver.cpp
@@ -16,7 +16,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-#ifdef WIN32
+#define __USE_MINGW_ANSI_STDIO 0
+#ifdef _MSC_VER  
 #include "stdafx.h"
 #endif
 #include "../include/jsonrpc-lean/jsonformathandler.h"
